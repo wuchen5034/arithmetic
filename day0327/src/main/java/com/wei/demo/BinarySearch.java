a5d3c993b98eac820ebea97bd7e75f8ebc889e79
wuchen <wuchen@wei.com>
2019-03-28 09:04:57 +0800

    二分查找改进写法：注意计算mid的时候，直接使用mid=(end+start)/2,如果end和start足够大的时候,可能会溢出。推荐使用 mid = start + (end-start)/2 or mid = start + (end-start)>>2
    比较的时候,先比较大于或者小于,因为大于小于的可能性要远远大于等于的时候，这样写出来的算法比较次数是最少的，虽然时间复杂度都是一样的。

--- day0327/src/main/java/com/wei/demo/BinarySearch.java
+++ day0327/src/main/java/com/wei/demo/BinarySearch.java
@@ -13,8 +13,9 @@ public class BinarySearch {
     public static void main(String[] args) {
         BinarySearch binarySearch = new BinarySearch();
         int[]  arr  = {1,2,2,4,5,5};
-        int target = 2;
+        int target = 5;
         System.out.println(binarySearch.getIndexByBinarySearch(arr,target));
+        System.out.println(binarySearch.refBinarySearch(arr,target));
     }
 
 
@@ -35,5 +36,44 @@ public class BinarySearch {
         }
         return -1;
     }
+
+
+    public int refBinarySearch(int[] arr,int key){
+        int start = 0;
+        int end = arr.length-1;
+
+        while (start<=end){
+            /**
+             * 如果写成(end+start+1)/2,end和start很大的时候,加法会溢出
+             * 推荐如下写法
+             * mid = start + (end - start) / 2 or mid = start + ((end - start) >> 1)
+             */
+            int mid = start + (end-start) / 2;
+
+            /**
+             * 建议先比较大于或者小于,因为大于小于的可能性要远远大于等于的时候,
+             * 这样写出来的算法比较次数是最少的，虽然时间复杂度都是一样的。
+             */
+            if (key < arr[mid]) {
+                end = mid -1;
+            } else if (key > arr[mid]) {
+                start = mid + 1;
+            }else {
+                return mid;
+            }
+
+
+        }
+
+
+
+        return  -1;
+
+    }
+
+
+
+
+
 
 }
